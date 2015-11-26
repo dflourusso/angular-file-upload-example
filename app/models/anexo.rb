abda670f3fc22f22204b5a6fc717e3151ccacecc
Daniel Fernando Lourusso <dflourusso@gmail.com>
2015-11-26 09:43:55 -0200

    annotate --exclude tests,fixtures,factories,serializers

--- app/models/anexo.rb
+++ app/models/anexo.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: anexos
+#
+#  id         :integer          not null, primary key
+#  data       :string
+#  anexo_id   :integer
+#  anexo_type :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Anexo < ActiveRecord::Base
 end
--- app/models/doc.rb
+++ app/models/doc.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: docs
+#
+#  id         :integer          not null, primary key
+#  nome       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Doc < ActiveRecord::Base
 end
